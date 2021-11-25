bb5d5a8eeaf86ead9a5d13b244240c44675f1a5b
Brady Trudel <bradytrudel@outlook.com>
2021-11-25 17:17:35 -0500

    WARNING: The Minecraft game is not fully built

---
+++ build/installer/install.bat
@@ -0,0 +1,5 @@
+@echo off
+title installing...
+
+python.exe -m pip install --upgrade pip
+pip install ursina
---
+++ build/installer/wizard.py
@@ -0,0 +1,13 @@
+import turtle
+import os
+
+turtle.title('Minecraft Wizard Setup')
+turtle.write('Installing...', font=('Arial', '22', 'bold'))
+turtle.hideturtle()
+os.system('install.bat')
+turtle.clearscreen()
+turtle.write('Done', font=('Arial', 22, 'bold'))
+turtle.hideturtle()
+
+
+turtle.mainloop()
