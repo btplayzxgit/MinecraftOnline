c8a4cb91d282a6786c24f385d6265eb9c9e01e9b
Brady Trudel <bradytrudel@outlook.com>
2021-11-25 17:32:51 -0500

    The clone is building... 9% So you should come back another time...

---
+++ RealMinecraft.py
@@ -0,0 +1,6 @@
+import os
+
+try:
+    import ursina
+except:
+    os.system('python build\\installer\\wizard.py')
--- build/installer/wizard.py
+++ build/installer/wizard.py
@@ -4,7 +4,7 @@ import os
 turtle.title('Minecraft Wizard Setup')
 turtle.write('Installing...', font=('Arial', '22', 'bold'))
 turtle.hideturtle()
-os.system('install.bat')
+os.system('build\\installer\\install.bat')
 turtle.clearscreen()
 turtle.write('Done', font=('Arial', 22, 'bold'))
 turtle.hideturtle()
